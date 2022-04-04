f5edffb000594311493240b4cc869952a32c326b
Krzysztof Szymek <63580276+krzysztofszymek@users.noreply.github.com>
2022-04-04 16:20:26 +0200

    Food Ordering App Add

--- src/components/myprojects/ProjectsData/ProjectsData.js
+++ src/components/myprojects/ProjectsData/ProjectsData.js
@@ -5,7 +5,13 @@ const ProjectsData = [
         img: '/img/projects/fastest-route.PNG',
         alt: 'Airports - Project',
         link: 'https://airports-fastest-route.netlify.app/',
-        
+    },
+    {
+        title: 'Food Ordering App',
+        description: 'Aplikacja do zamawiania jedzenia z możliwościa dodawania do koszyka stworzona w trakcie kursu React',
+        img: '/img/projects/food-ordering-app.PNG',
+        alt: 'Food ordering App',
+        link: 'https://ks-food-ordering-app.netlify.app/',
     }
 ]
 
